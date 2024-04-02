6090b58bb164575acb3d67f949bb935f7509c9ee
Justin Cheung <justin.cheung.1@stonybrook.edu>
2024-04-02 16:52:04 -0400

    My first submission

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,108 @@
+//Justin Cheung SBU ID: 115923252
+//Lab 8 Part 1
 
+#include <stdio.h>
+
+int my_isalpha (char c){
+    
+    if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        return 1;
+    else
+        return 0;
+}
+
+int my_isdigit (char c){
+    
+    if(c >= '0' && c <= '9')
+        return 1;
+    else
+        return 0;
+}
+
+int my_is_upper (char c){
+    
+    if(c >= 'A' && c <= 'Z')
+        return 1;
+    else
+        return 0;
+}
+
+char my_to_upper (char c){
+    
+    if(c >= 'a' && c <= 'z')
+        return c - 'a' + 'A';
+    else
+        return c;
+}
+
+char my_to_lower (char c){
+    
+    if(c >= 'A' && c <= 'Z')
+        return c - 'A' + 'a';
+    else
+        return c;
+}
+
+int main(void){
+    char c;
+    char d;
+    
+    for(int i = 0; i < 2; i++){
+        
+        printf("ch: ");
+        scanf("%c",&c);
+        getchar();
+        d = my_isalpha(c);
+        
+            if(d == 1)
+                printf("%c is an uppercase or lower case letter.\n", c);
+            else
+                printf("%c is not an uppercase or lower case letter.\n", c);
+    }
+    
+    for(int i = 0; i<2; i++){
+       
+        printf("digit: ");
+        scanf("%c", &c);
+        getchar();
+        d = my_isdigit(c);
+        
+        if(d == 1)
+            printf("%c is a digit\n", c);
+        else
+            printf("%c is not a digit\n", c);
+    }
+    
+    for(int i = 0; i<2; i++){
+        
+        printf("Upper: ");
+        scanf("%c", &c);
+        getchar();
+        d = my_is_upper(c);
+        
+        if(d == 1)
+            printf("%c is uppercase\n", c);
+        else
+            printf("%c is not uppercase\n", c);
+    }
+    
+    for (int i = 0; i<2; i++){
+        
+        printf("Enter a character: ");
+            scanf(" %c", &c);
+
+            char upper = my_to_upper(c);
+
+            printf("The uppercase of %c is %c\n", c, upper);
+    }
+   
+    for (int i = 0; i<2; i++){
+        
+        printf("Enter a character: ");
+            scanf(" %c", &c);
+
+            char lower = my_to_lower(c);
+
+            printf("The lowercase of %c is %c\n", c, lower);
+    }
+}
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,99 @@
+//Justin Cheung SBU ID: 115923252
+//Lab 8 Part 2
+
+#include <stdio.h>
+#include <string.h>
+
+#define LIM 32
+
+int my_strcmp(char str1[], char str2[]) {
+    int i = 0;
+    
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            
+            return 1;
+        }
+        i++;
+    }
+    if (str1[i] == '\0' && str2[i] == '\0')
+        return 0;
+    else
+        return 1;
+    
+}
+
+int my_strncmp(char str1[],char str2[], int n) {
+    int i = 0;
+
+    while (str1[i] != '\0' && str2[i] != '\0' && i < n) {
+        if (str1[i] != str2[i]) {
+            return 1;
+        }
+        i++;
+    }
+
+    if ((str1[i] == '\0' && str2[i] == '\0') || i == n)
+        return 0;
+    else
+        return 1;
+}
+
+char *my_strcpy (char dest[], char src[]){
+    
+    int i = 0;
+    while(src[i] != '\0'){
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+    
+int main(void){
+    
+    char s1 [LIM], s2 [LIM];
+    int t;
+   
+    for(int i = 0; i < 2; i++){
+        printf("s1: ");
+        scanf("%s", s1); getchar;
+        
+        printf("s2: ");
+        scanf("%s", s2); getchar;
+        
+        t = my_strcmp (s1, s2);
+        
+        if(t == 0)
+            printf("%s and %s are equal\n", s1, s2);
+        else
+            printf("%s and %s are not equal\n", s1, s2);
+    }
+    
+    for(int i = 0; i < 2; i++){
+        char str1[LIM], str2[LIM];
+        int n;
+        
+        printf("Enter the first string: ");
+        scanf("%s", str1);
+        
+        printf("Enter the second string: ");
+        scanf("%s", str2);
+        
+        printf("Enter the value of n: ");
+        scanf("%d", &n);
+        
+        int result = my_strncmp(str1, str2, n);
+        
+        if (result == 0)
+            printf("The first %d characters of the strings are equal.\n", n);
+        else
+            printf("The first %d characters of the strings are not equal.\n", n);
+        
+    }
+        return 0;
+        
+        
+    
+}
 
